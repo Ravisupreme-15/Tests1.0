61a2f2e02f0c864a36f30f98fdffac628c47e1bd
mohan_chandra <129154398+Ravisupreme-15@users.noreply.github.com>
2024-04-01 15:31:00 +0530

    Update Calculus.java

--- src/main/java/org/example/Calculus.java
+++ src/main/java/org/example/Calculus.java
@@ -29,4 +29,8 @@ public class Calculus {
     public static int max(int a, int b) {
         return Math.max(a,b);
     }
+
+     public static int min(int a, int b) {
+        return Math.min(a,b);
+    }
 }
